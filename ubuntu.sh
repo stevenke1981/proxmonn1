266f8b9e8b49ebe88182d5cd486407d3ba8aae0a
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2024-03-22 09:38:05 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 檢查是否為 root 用戶
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
 fi
 
 # 提示輸入期望的 vmid 範圍(1-999)
@@ -12,10 +12,12 @@ read -p "Enter the desired vmid range (1-999): " vmid_range
 # 提示輸入虛擬機名稱
 read -p "Enter a name for the new VM: " vm_name
 
+# 預設映像檔名稱
+imagename="jammy-server-cloudimg-amd64.img"
+
 # 在指定範圍內找到一個可用的 vmid
-for ((i=$vmid_range; i<1000; i++))
-do
-  vmid=$(qm status $i | grep $i)
+for ((i=$vmid_range; i<1000; i++)); do
+  vmid=$(qm status $i 2>/dev/null | grep -w $i)
   if [ -z "$vmid" ]; then
     vmid=$i
     break
@@ -28,17 +30,22 @@ if [ -z "$vmid" ]; then
   exit 1
 fi
 
-# 使用指定的 vmid 創建虛擬機
-qm create $vmid --name $vm_name --memory 2048 --net0 virtio,bridge=vmbr0
+# 檢查映像檔是否存在
+if [ -f "$imagename" ]; then
+  echo "Found existing $imagename, using it."
+else
+  # 下載 Ubuntu 22.04 雲端映像檔
+  wget "https://cloud-images.ubuntu.com/jammy/current/$imagename"
+fi
 
-# 導入 Ubuntu 22.04 雲端映像檔
-wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
+# 使用指定的 vmid 創建虛擬機
+qm create $vmid --name "$vm_name" --memory 2048 --net0 virtio,bridge=vmbr0
 
 # 將雲端映像檔設為虛擬機的虛擬硬碟
-qm importdisk $vmid jammy-server-cloudimg-amd64.img local-zfs
+qm importdisk $vmid "$imagename" local
 
 # 設定虛擬機啟動時從雲端映像檔開機
-qm set $vmid --scsi0 local-zfs:vm-$vmid-disk-0.qcow2
+qm set $vmid --scsi0 local:vm-$vmid-disk-0.qcow2
 
 # 設定虛擬機網路
 qm set $vmid --ipconfig0 ip=dhcp
