897414edbf4a8641dbe6aef4ceb5c527bee5d586
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2024-03-22 09:31:20 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,34 +1,53 @@
 #!/bin/bash
 
-# 判斷是否為 root 使用者
+# 檢查是否為 root 用戶
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-# 設定虛擬機器名稱
+# 提示輸入期望的 vmid 範圍(1-999)
+read -p "Enter the desired vmid range (1-999): " vmid_range
+
+# 提示輸入虛擬機名稱
 read -p "Enter a name for the new VM: " vm_name
 
-# 建立虛擬機器
-qm create $vm_name --memory 2048 --net0 virtio,bridge=vmbr0
+# 在指定範圍內找到一個可用的 vmid
+for ((i=$vmid_range; i<1000; i++))
+do
+  vmid=$(qm status $i | grep $i)
+  if [ -z "$vmid" ]; then
+    vmid=$i
+    break
+  fi
+done
+
+# 如果在範圍內沒有找到可用的 vmid，則退出
+if [ -z "$vmid" ]; then
+  echo "No available vmid found in the specified range."
+  exit 1
+fi
+
+# 使用指定的 vmid 創建虛擬機
+qm create $vmid --name $vm_name --memory 2048 --net0 virtio,bridge=vmbr0
 
 # 導入 Ubuntu 22.04 雲端映像檔
 wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
 
-# 將雲端映像檔設為虛擬機器的虛擬硬碟
-qm importdisk $vm_name jammy-server-cloudimg-amd64.img local-zfs
+# 將雲端映像檔設為虛擬機的虛擬硬碟
+qm importdisk $vmid jammy-server-cloudimg-amd64.img local-zfs
 
-# 設定虛擬機器啟動時從雲端映像檔開機
-qm set $vm_name --scsi0 local-zfs:vm-$vm_name-disk-0.qcow2
+# 設定虛擬機啟動時從雲端映像檔開機
+qm set $vmid --scsi0 local-zfs:vm-$vmid-disk-0.qcow2
 
-# 設定虛擬機器網路
-qm set $vm_name --ipconfig0 ip=dhcp
+# 設定虛擬機網路
+qm set $vmid --ipconfig0 ip=dhcp
 
-# 允許 VRDE (虛擬機器遠端桌面連線)
-qm set $vm_name --vga qxl
-qm set $vm_name --machine pc-q35-5.2
+# 允許 VRDE (虛擬機遠端桌面連線)
+qm set $vmid --vga qxl
+qm set $vmid --machine pc-q35-5.2
 
-# 啟動虛擬機器
-qm start $vm_name
+# 啟動虛擬機
+qm start $vmid
 
-echo "Ubuntu 22.04 VM $vm_name has been created and started."
+echo "Ubuntu 22.04 VM $vm_name (ID: $vmid) has been created and started."
